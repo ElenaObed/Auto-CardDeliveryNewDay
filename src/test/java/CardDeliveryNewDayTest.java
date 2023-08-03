b25b89707fe9d5b830869a7d1541061a3cf96ee3
Elena Obed <elena_sergeevna137@mail.ru>
2023-08-03 10:53:53 +0300

    Initial commit

---
+++ src/test/java/CardDeliveryNewDayTest.java
@@ -0,0 +1,47 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryNewDayTest {
+    @BeforeEach
+    void setUp () {
+        open("http://localhost:9999");
+
+    }
+
+    @Test
+    @DisplayName("Should successful plan meeting")
+    void shouldTestNewDate() {
+
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        String date = DataGenerator.generaterDate(3);
+        String secondDate = DataGenerator.generaterDate(7);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + date)).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(secondDate);
+        $(byText("Запланировать")).click();
+        $("[data-test-id = ]");
+    }
+
+
+    }
+
